c23463d4a2aa3a265f22840d5ce61fe8e41ea20e
花本敬太 <hanamotokeita@hanamottanoiMac.sensyu.ne.jp>
2021-03-14 16:28:16 +0900

    p1

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,20 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('hello', function () {
+    return '
+    <html>
+    <body>
+    <h1>Hello</h1>
+    <p>これはサンプルページです</p>
+    <p>今日はこれからlaravelの練習をしてきますので、よろしくお願い致します。</p>
+    <input type="text" method="post" placeholder="ここに入力してね">
+    <input type="checkbox">
+    <input type="radio">
+    <input type="password">
+    <input type="submit" value="送信" >
+    </body>
+    </html>
+    ';
+});
