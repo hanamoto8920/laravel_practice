5c80ab9f1d80874fcf31df3b2668e402cb595b4d
花本敬太 <hanamotokeita@hanamottanoiMac.sensyu.ne.jp>
2021-03-17 17:36:44 +0900

    comp

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -4,14 +4,19 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Http\Requests\HelloRequest;
+use App\Models\Person;
 
 use Illuminate\Support\Facades\DB;
 
 class HelloController extends Controller
 {
     public function index(Request $request) {
-        $items = DB::select('select * from people');
-        return view('hello.index', ['items' => $items]);
+        $sort = $request->sort;
+        
+        // $items = DB::table('people')->orderBy('age','asc')->simplePaginate(2);
+        $items = Person::orderBy('sort', 'asc')->simplePaginate(3);
+        $param = ['items' => $items, 'sort'=>$sort];
+        return view('hello.index',$param);
     }
     
     public function rest(Request $request) {
@@ -20,7 +25,7 @@ class HelloController extends Controller
     
     public function ses_get(Request $request) {
         $ses_data = $request->session()->get('msg');
-        return view('hello.session', ['session_data' =>$sesdata]);
+        return view('hello.session', ['session_data' =>$ses_data]);
     }
 
     public function ses_put(Request $request) {
--- resources/views/hello/index.blade.php
+++ resources/views/hello/index.blade.php
@@ -1,4 +1,12 @@
 @extends ('layouts.helloapp')
+<style>
+  .pagination {font-size:10pt; }
+  .pagination li{display: inline-block;}
+  tr th a:link {color: white;}
+  tr th a:visited {color: white;}
+  tr th a:hover {color: white;}
+  tr th a:active {color: white;}
+</style>
 
 @section('title', 'Index')
 
@@ -8,12 +16,24 @@
 @endsection
 
 @section('content')
-  @foreach($items as $item)
-  <p>{{$item->name}}</p>
-  <p>{{$item->mail}}</p>
-  <p>{{$item->age}}</p>
-  @endforeach
+  <table>
+    <tr>
+    <th><a href="/hello?sort=name">name</a></th>
+    <th><a href="/hello?sort=mail">mail</a></th>
+    <th><a href="/hello?sort=age">age</a></th>
+    </tr>
+    @foreach($items as $item)
+    <tr>
+      <td>{{$item->name}}</td>
+      <td>{{$item->mail}}</td>
+      <td>{{$item->age}}</td>
+    </tr>
+    @endforeach
+  </table>
+  {{ $items->appends(['sort' => $sort])->links()}}
 @endsection
+
+<a href="/person/add">追加</a>
 
 @section('footer')
 copyright 2021 hanamoto.
